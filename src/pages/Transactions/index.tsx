e980c4d91ccc5f820ce725a2274a907a113b9945
raphaelc484 <raphaelc484@hotmail.com>
2022-11-22 16:14:35 -0300

    add trabela + search

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,41 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import {
+  PriceHightLight,
+  TransactionContainer,
+  TransactionTable,
+} from "./styles";
 
 export function Transaction() {
   return (
     <div>
       <Header />
+      <Summary />
+
+      <TransactionContainer>
+        <SearchForm />
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHightLight variant="income">R$ 12.000,00</PriceHightLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHightLight variant="outcome">- R$ 59,00</PriceHightLight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
     </div>
   );
 }
